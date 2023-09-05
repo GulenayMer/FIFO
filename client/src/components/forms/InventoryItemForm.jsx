7387ba4e4c1384681ae7fc61801ca9353c761a84
GulenayMer <mervegulenay@gmail.com>
2023-09-05 21:47:51 +0200

    dishtable

--- client/src/components/forms/InventoryItemForm.jsx
+++ client/src/components/forms/InventoryItemForm.jsx
@@ -178,10 +178,9 @@ const InventoryItemForm = () => {
           </div>
         </div>
         <div className="">
-
           <button className="flex w-full justify-center rounded-md bg-emerald-800 px-3 py-1.5 
 		  text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-700 focus-visible:outline
-		   focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+		   focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Add Item
           </button>
         </div>
