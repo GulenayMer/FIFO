ef321079bbd13fe88d1d6d4853487836e82d34dc
Max Horstmann <mahor575@gmail.com.com>
2023-09-06 11:21:29 +0200

    not sure why I had to commit

--- client/src/pages/private/inventoryItemPage/InventoryItems.jsx
+++ client/src/pages/private/inventoryItemPage/InventoryItems.jsx
@@ -1,13 +1,16 @@
 import InventoryItemForm from '../../../components/forms/InventoryItemForm';
 import DefaultLayout from '../../../components/layout/DefaultLayout';
 import InventoryItemTable from './InventoryItemTable';
+import UpdateInventoryItem from './UpdateInventoryItem';
 
 const InventoryItems = () => {
   return (
     <DefaultLayout>
       <div className="flex align-middle justify-center items-center flex-col w-full py-5 px-5 ">
         <InventoryItemForm />
+
         <InventoryItemTable />
+        <UpdateInventoryItem />
       </div>
     </DefaultLayout>
   );
